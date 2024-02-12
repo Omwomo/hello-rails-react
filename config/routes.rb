db653543f97acaa896c539f3e0acb601d8512f90
Omwomo <eugeneoduor79@gmail.com>
2024-02-12 15:40:42 +0300

    Changed root route to random greeting.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'root#index'
+  root 'greetings#random_greeting'
 
   get '/random_greeting', to: 'greetings#random_greeting'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
