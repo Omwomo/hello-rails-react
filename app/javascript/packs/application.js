20eaa5c09983c4be4d46d68c0b50e49839817600
Omwomo <eugeneoduor79@gmail.com>
2024-02-12 15:39:34 +0300

    Imported main App component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,6 @@
-import '../components/App';
+import App from '../components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
 
 ReactDOM.render(
   <App/>,
