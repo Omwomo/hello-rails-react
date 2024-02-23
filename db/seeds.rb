d7443ce626ca73e68bf34134c1df85b9eb5f0cd0
Omwomo <eugeneoduor79@gmail.com>
2024-02-23 10:00:32 +0300

    Created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Message.create(content: "Hey there!")
+Message.create(content: "Good day!")
+Message.create(content: "Greetings")
+Message.create(content: "Welcome!")
+Message.create(content: "Hello!")
